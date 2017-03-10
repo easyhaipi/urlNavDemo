69bb49d4b6bdf5f2458065110b4322bbe3879ff0
wangqingsong <wangqs@cloud-young.com>
2017-03-10 18:31:30 +0800

    Initial Commit

---
+++ urlNavDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  urlNavDemo
+//
+//  Created by hulinEasy on 2017/3/10.
+//  Copyright © 2017年 easy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ urlNavDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  urlNavDemo
+//
+//  Created by hulinEasy on 2017/3/10.
+//  Copyright © 2017年 easy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
